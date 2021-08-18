df6a70d84c4721dc1c791768eff0c181242eeb48
Eco Galileo <57631939+ecogalileo@users.noreply.github.com>
2021-08-19 00:50:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// import ImgCardDisplay from './components/ImgCardDisplay';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
 const App = () => {
   return (
-    // <div style={{ height: '100vh' }}>
     <div>
       <Header />
       <Main />
